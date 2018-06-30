30e171798684f17162339a2e2e1deb1f3dbbe51b
kapieyow <33786261+kapieyow@users.noreply.github.com>
2018-06-30 18:57:13 -0400

    Initial setup, routing, css, login and top nav shells

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    TopNavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/environments/version.ts
@@ -0,0 +1,3 @@
+export const VERSION = {
+    "version": "0.0.1"
+};
