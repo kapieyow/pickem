9660854a21f45be6b4046b454bb559956c26fb8e
kapieyow <33786261+kapieyow@users.noreply.github.com>
2018-09-03 21:19:30 -0400

    db backup done

--- pickem-db-backup.py
+++ pickem-db-backup.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-
 import argparse
 import datetime
+import subprocess
 
 parser = argparse.ArgumentParser(description='Backup a Postgres db')
 parser.add_argument('-db', '--database', required=True, help='Database name')
-parser.add_argument('-U', '--database_user', required=True, help='Database user')
 args = parser.parse_args()
 
 now = datetime.datetime.now()
-
-print(now.isoformat())
+output_filename = now.strftime('%Y%m%d-%H%M%S') + "-" + args.database + ".sql"
+subprocess.call("pg_dump " + args.database + " > " + output_filename, shell=True)
+print("DB dumped to file: " + output_filename)
