351a9360ea696f916cbb82377e1faf411c9dfc5f
kapieyow <33786261+kapieyow@users.noreply.github.com>
2018-09-15 22:11:01 -0400

    add prod settings and build / deploy

--- PickEmServer/pickem_publish_arm_prod.bat
+++ PickEmServer/pickem_publish_arm_prod.bat
@@ -1,12 +1,12 @@
 ﻿REM base version - udpate this one
-REM  0.8.X - web and server "running" first running release to test
-SET ASSM_VERSION=0.11.30-linarm
+REM  1.0.X - first deploy to prod
+SET ASSM_VERSION=1.0.31-linarm
 
 REM append date-time
 SET ASSM_VERSION=%ASSM_VERSION%-%date:~4,2%-%date:~7,2%-%date:~10,4%-%time:~0,2%%time:~3,2%%time:~6,2%
 REM remove space when time is 1 to 9 AM
 SET ASSM_VERSION=%ASSM_VERSION: =%
 
-dotnet publish -c Test -r linux-arm /p:Version=%ASSM_VERSION%
+dotnet publish -c Production -r linux-arm /p:Version=%ASSM_VERSION%
 
 pause
