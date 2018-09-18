b1f70aeca8479c89447e042c6a55f3535ea0578b
kapieyow <33786261+kapieyow@users.noreply.github.com>
2018-09-17 22:30:40 -0400

    hacked league change

--- src/app/test-driver/test-driver.component.ts
+++ src/app/test-driver/test-driver.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { LoggerService } from '../sub-system/services/logger.service';
+import { Router } from '@angular/router';
+import { LeagueService } from '../sub-system/services/league.service';
+import { LoggerService } from '../sub-system/services//logger.service';
+import { StatusService } from '../sub-system/services/status.service';
 import { Log } from '../sub-system/models/api/log';
 
 @Component({
@@ -9,7 +12,7 @@ import { Log } from '../sub-system/models/api/log';
 })
 export class TestDriverComponent implements OnInit {
 
-  constructor(private logger: LoggerService) { }
+  constructor(private router: Router, private leagueService: LeagueService, private logger: LoggerService, private statusService: StatusService) { }
 
   ngOnInit() {
   }
@@ -20,6 +23,42 @@ export class TestDriverComponent implements OnInit {
   error(logMessage: string) { this.logger.error(logMessage); }
   wtf(logMessage: string) { this.logger.wtf(logMessage); }
   
+  changeToLeague(league: string)
+  {
+    this.statusService.leagueCode = league;
+    
+    this.leagueService.loadPlayers(this.statusService.seasonCode, this.statusService.leagueCode).subscribe(response => 
+      {     
+          this.leagueService.loadWeeks(this.statusService.seasonCode, this.statusService.leagueCode).subscribe(response => 
+            { 
+              this.leagueService.loadPlayerScoreboard(
+                this.statusService.seasonCode, 
+                this.statusService.leagueCode, 
+                this.statusService.weekNumberFilter,
+                this.statusService.playerTagFilter);
+
+              this.leagueService.loadWeekScoreboard(
+                this.statusService.seasonCode, 
+                this.statusService.leagueCode, 
+                this.statusService.weekNumberFilter
+                );
+
+              this.leagueService.loadLeagueScoreboard(
+                this.statusService.seasonCode, 
+                this.statusService.leagueCode
+                );
+
+              // user fully setup go to player view
+              this.statusService.userLoggedInAndInitialized = true;
+              this.router.navigate(['/player'], { skipLocationChange: true });   
+            },
+            errors => { this.wtf(errors); }
+          );
+      },
+      errors => { this.wtf(errors); }
+    );
+
+  }
 
   logs: Log[];
   errorMessages: string[] = [];
