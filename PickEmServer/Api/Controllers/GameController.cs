53074675199d973f4ada17a91526eb395e3df155
kapieyow <33786261+kapieyow@users.noreply.github.com>
2018-09-30 21:17:42 -0400

    cut the /private api and put authorizes on it.

--- PickEmServer/Api/Controllers/GameController.cs
+++ PickEmServer/Api/Controllers/GameController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PickEmServer.Api.Models;
@@ -19,43 +20,49 @@ namespace PickEmServer.Api.Controllers
             _gameService = gameService;
         }
 
+        [Authorize]
         [HttpGet]
-        [Route("api/private/{SeasonCode}/{WeekNumber}/games_in_any_league")]
+        [Route("api/{SeasonCode}/{WeekNumber}/games_in_any_league")]
         public async Task<List<Game>> ReadGamesInAnyLeague(string SeasonCode, int WeekNumber)
         {
             return await _gameService.ReadGamesInAnyLeague(SeasonCode, WeekNumber);
         }
 
+        [Authorize]
         [HttpGet]
-        [Route("api/private/{SeasonCode}/{WeekNumber}/games")]
+        [Route("api/{SeasonCode}/{WeekNumber}/games")]
         public async Task<List<Game>> ReadGames(string SeasonCode, int WeekNumber)
         {
             return await _gameService.ReadGames(SeasonCode, WeekNumber);
         }
 
+        [Authorize]
         [HttpPost]
-        [Route("api/private/{SeasonCode}/{WeekNumber}/games")]
+        [Route("api/{SeasonCode}/{WeekNumber}/games")]
         public async Task<Game> AddGame(string SeasonCode, int WeekNumber, [FromBody] GameAdd newGame)
         {
             return await _gameService.AddGame(SeasonCode, WeekNumber, newGame);
         }
 
+        [Authorize]
         [HttpPut]
-        [Route("api/private/{SeasonCode}/{WeekNumber}/games/{GameId}")]
+        [Route("api/{SeasonCode}/{WeekNumber}/games/{GameId}")]
         public async Task<Game> UpdateGame(string SeasonCode, int WeekNumber, int GameId, [FromBody] GameUpdate gameUpdates)
         {
             return await _gameService.UpdateGame(SeasonCode, WeekNumber, GameId, gameUpdates);
         }
 
+        [Authorize]
         [HttpPut]
-        [Route("api/private/{SeasonCode}/{WeekNumber}/games/{GameId}/spread")]
+        [Route("api/{SeasonCode}/{WeekNumber}/games/{GameId}/spread")]
         public async Task<Game> UpdateSpread(string SeasonCode, int WeekNumber, int GameId, [FromBody] SpreadUpdate spreadUpdates)
         {
             return await _gameService.UpdateSpread(SeasonCode, WeekNumber, GameId, spreadUpdates);
         }
 
+        [Authorize]
         [HttpPut]
-        [Route("api/private/{SeasonCode}/{WeekNumber}/games/{GameId}/spread/lock")]
+        [Route("api/{SeasonCode}/{WeekNumber}/games/{GameId}/spread/lock")]
         public async Task<Game> UpdateSpread(string SeasonCode, int WeekNumber, int GameId)
         {
             return await _gameService.LockSpread(SeasonCode, WeekNumber, GameId);
