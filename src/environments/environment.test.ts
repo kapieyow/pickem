392d85d13e519c83b728e2bcdd5ba89dbddd2a98
kapieyow <33786261+kapieyow@users.noreply.github.com>
2018-10-20 10:44:08 -0400

    web socket conf added for test

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -2,4 +2,5 @@ export const environment = {
     production: false,
     pickemRestServerBaseUrl: "https://streamhead.duckdns.org/p-api/tst/api",
     pickemBaseDomain: "streamhead.duckdns.org",
+    pickemWebSocketUrl: "ws://streamhead.duckdns.org/p-ws/tst/ws",
   };
