ad1c91669a72aa02c08009fcc4ead3f0e69f0cca
kapieyow <33786261+kapieyow@users.noreply.github.com>
2018-11-07 20:18:10 -0500

    Login complete

--- pickem-droid/app/src/main/java/com/example/kip/pickem_droid/LoginActivity.kt
+++ pickem-droid/app/src/main/java/com/example/kip/pickem_droid/LoginActivity.kt
@@ -6,18 +6,17 @@ import android.annotation.TargetApi
 import android.support.v7.app.AppCompatActivity
 import android.os.Build
 import android.os.Bundle
-import android.text.TextUtils
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.widget.TextView
 import com.example.kip.pickem_droid.services.AuthService
 import com.example.kip.pickem_droid.services.models.UserLoggedIn
-import com.google.gson.Gson
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 
 import kotlinx.android.synthetic.main.activity_login.*
 import okhttp3.ResponseBody
+import org.json.JSONArray
 import org.json.JSONObject
 import retrofit2.HttpException
 
@@ -34,7 +33,7 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
         // Set up the login form.
 
-        password_textbox.setOnEditorActionListener(
+        txtPassword.setOnEditorActionListener(
             TextView.OnEditorActionListener
             { _, id, _ ->
                 if (id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL)
@@ -46,7 +45,7 @@ class LoginActivity : AppCompatActivity() {
             }
         )
 
-        login_button.setOnClickListener { attemptLogin() }
+        btnLogin.setOnClickListener { attemptLogin() }
     }
 
 
@@ -57,48 +56,23 @@ class LoginActivity : AppCompatActivity() {
      */
     private fun attemptLogin()
     {
-        // Reset errors.
-        username_textbox.error = null
-        password_textbox.error = null
+        // clear any previous issues
+        lblErrorMessages.text = ""
 
         // Store values at the time of the login attempt.
-        val username = username_textbox.text.toString()
-        val password = password_textbox.text.toString()
+        val username = txtUsername.text.toString()
+        val password = txtPassword.text.toString()
 
-        var cancel = false
-        var focusView: View? = null
-
-        if ( TextUtils.isEmpty(username) )
-        {
-            username_textbox.error = "User Name is required"
-            focusView = username_textbox
-            cancel = true
-        }
-
-        if ( TextUtils.isEmpty(password) )
-        {
-            password_textbox.error = "Password is required"
-            focusView = password_textbox
-            cancel = true
-        }
-
-        if (cancel)
-        {
-            focusView?.requestFocus()
-        }
-        else
-        {
-            // Show a progress spinner, and kick off a background task to
-            // perform the user login attempt.
-            showProgress(true)
-            _authService.startLogin(username, password)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                    { result -> successfulLogin(result) },
-                    { error -> showServiceError(error) }
-                )
-        }
+        // Show a progress spinner, and kick off a background task to
+        // perform the user login attempt.
+        showProgress(true)
+        _authService.startLogin(username, password)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { result -> successfulLogin(result) },
+                { error -> showServiceError(error) }
+            )
     }
 
     private fun showServiceError(error: Throwable)
@@ -108,7 +82,7 @@ class LoginActivity : AppCompatActivity() {
             if ( error.code() == 401 )
             {
                 // not authorized, not a error, bad user or password
-                lblTempOut.text = "User name and password are not valid"
+                lblErrorMessages.text = "User name and password are not valid"
             }
             else if ( error.code() == 400 )
             {
@@ -120,25 +94,40 @@ class LoginActivity : AppCompatActivity() {
                     // if the server sent back structured data, it will be first level properties each with an array of messages.
                     // all we care about here is the messages
                     var errorBody: ResponseBody? = error.response().errorBody()
-                    var json = JSONObject(errorBody?.toString())
-                    lblTempOut.text = "HMMMMMMMMMM"
+                    var errorJsonAsString = errorBody?.string()
+                    var errorJson = JSONObject(errorJsonAsString)
+
+                    var outputMessages: String = ""
+
+                    for ( key in errorJson.keys() )
+                    {
+                        // ASSuming value is an array
+                        var messages = (errorJson[key] as JSONArray)
+
+                        for ( i in 0..messages.length() - 1 )
+                        {
+                            outputMessages = "${outputMessages}\n${messages[i]}"
+                        }
+                    }
+
+                    lblErrorMessages.text = outputMessages;
                 }
                 catch (e: Exception)
                 {
-                    lblTempOut.text = error.message
+                    lblErrorMessages.text = error.message
                 }
             }
         }
         else
         {
-            lblTempOut.text = error.message
+            lblErrorMessages.text = error.message
         }
         showProgress(false)
     }
 
     private fun successfulLogin(userLoggedIn: UserLoggedIn)
     {
-        lblTempOut.text = """
+        lblErrorMessages.text = """
             defaultLeagueCode: ${userLoggedIn.defaultLeagueCode}
             email: ${userLoggedIn.email}
             token: ${userLoggedIn.token}
@@ -157,14 +146,14 @@ class LoginActivity : AppCompatActivity() {
 
         val shortAnimTime = resources.getInteger(android.R.integer.config_shortAnimTime).toLong()
 
-        login_progress.visibility = if (show) View.VISIBLE else View.GONE
-        login_progress
+        pbLoginProgress.visibility = if (show) View.VISIBLE else View.GONE
+        pbLoginProgress
             .animate()
             .setDuration(shortAnimTime)
             .alpha((if (show) 1 else 0).toFloat())
             .setListener(object : AnimatorListenerAdapter() {
                 override fun onAnimationEnd(animation: Animator) {
-                    login_progress.visibility = if (show) View.VISIBLE else View.GONE
+                    pbLoginProgress.visibility = if (show) View.VISIBLE else View.GONE
                 }
             })
 
