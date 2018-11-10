b54ee9708d88bcf7d0623598e3648428fed94404
kapieyow <33786261+kapieyow@users.noreply.github.com>
2018-11-10 11:46:33 -0500

    Stubbed most activities and fragments. Not wired up.

---
+++ pickem-droid/app/src/main/java/com/example/kip/pickem_droid/ScoreboardsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kip.pickem_droid
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ScoreboardsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scoreboards)
+    }
+}
