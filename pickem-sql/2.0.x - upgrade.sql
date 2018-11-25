d9fe238bc62357b15f1ca9f8ece4028c65bee9e9
kapieyow <33786261+kapieyow@users.noreply.github.com>
2018-11-25 16:32:11 -0500

    tweak to upgrade SQL

--- pickem-sql/2.0.x - upgrade.sql
+++ pickem-sql/2.0.x - upgrade.sql
@@ -4,7 +4,7 @@ BEGIN;
 UPDATE 
     public.mt_doc_leaguedata 
 SET 
-    data = jsonb_set(jsonb_set(jsonb_set(data, '{PickemScoringType}', '"AllWinsOnePoint"'), '{CurrentWeekRef}', '12'), '{NcaaSeasonCodeRef}', '"2018"');
+    data = jsonb_set(jsonb_set(jsonb_set(data, '{PickemScoringType}', '"AllWinsOnePoint"'), '{CurrentWeekRef}', '13'), '{NcaaSeasonCodeRef}', '"2018"');
 
 DROP FUNCTION public.mt_insert_systemsettingsdata(doc jsonb, docdotnettype character varying, docid integer, docversion uuid);
 DROP FUNCTION public.mt_upsert_systemsettingsdata(doc jsonb, docdotnettype character varying, docid integer, docversion uuid);
