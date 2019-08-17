5673b690d42e6ffaf17dcaf27a9b4721c14439bb
kapieyow <33786261+kapieyow@users.noreply.github.com>
2019-08-17 14:44:57 -0400

    separated CLI from core scripting

--- pickem-scripts/pickem.py
+++ pickem-scripts/pickem.py
@@ -2,10 +2,8 @@
  
 import argparse
 import configparser
-import datetime
-import json
-import requests
 import time
+import pickemCore
 import pickemLogger
 import pickemApiClient
 import pickemSynchGames
@@ -13,19 +11,7 @@ import pickemUpdateSpreads
 import pickemUpdateTeams
 
 # "configs"
-VERSION = "2.0.34"
-PICKEM_INI = "pickem-settings.ini"
-
-# globals
-class Settings:
-    # from INI
-    MinLogLevelToApi = ""
-    PickemAdminUsername = ""
-    PickemAdminPassword = ""
-    PickemServerBaseUrl = ""
-
-settings = Settings()
-apiClient = None
+VERSION = "2.1.35"
 
 #=====================================
 # sub command methods
@@ -37,18 +23,18 @@ def setupBowls(args):
     pickemSeasonCode = "18"
 
     weekNumber = 20
-    apiClient.insertGame(pickemSeasonCode, weekNumber, 401032087, "2019-01-07T20:00:00.000Z", "College Football Playoff National Championship Presented By AT&T", "false", "clemson", "alabama")
+    core.apiClient.insertGame(pickemSeasonCode, weekNumber, 401032087, "2019-01-07T20:00:00.000Z", "College Football Playoff National Championship Presented By AT&T", "false", "clemson", "alabama")
 
-    logger.info("18 Bowl Championship setup!")
+    core.logger.info("18 Bowl Championship setup!")
 
 def setLeagueGame(args):
     gamesSet = 0
 
     for leagueCode in args.league_codes:
-        apiClient.setLeagueGame(leagueCode, args.week, args.game_id, args.game_win_points)
+        core.apiClient.setLeagueGame(leagueCode, args.week, args.game_id, args.game_win_points)
         gamesSet = gamesSet + 1
 
-        logger.info("Set (" + str(gamesSet) + ") games for league (" + leagueCode + ") in week (" + str(args.week) + ")")
+        core.logger.info("Set (" + str(gamesSet) + ") games for league (" + leagueCode + ") in week (" + str(args.week) + ")")
 
 def setLeagueGames(args):
     gamesSet = 0
@@ -56,18 +42,18 @@ def setLeagueGames(args):
     for leagueCode in args.league_codes:
         for gameId in args.game_ids:
             # no win points can be sent in, when setting multiple games at a time, so default to "1"
-            apiClient.setLeagueGame(leagueCode, args.week, gameId, 1)
+            core.apiClient.setLeagueGame(leagueCode, args.week, gameId, 1)
             gamesSet = gamesSet + 1
 
-        logger.info("Set (" + str(gamesSet) + ") games for league (" + leagueCode + ") in week (" + str(args.week) + ")")
+        core.logger.info("Set (" + str(gamesSet) + ") games for league (" + leagueCode + ") in week (" + str(args.week) + ")")
 
 def setupLeagueWeek(args):
     for leagueCode in args.league_codes:
-        apiClient.updateLeague(leagueCode, args.week)
-        logger.info("Set week to (" + str(args.week) + ") for league (" + leagueCode + ")")
+        core.apiClient.updateLeague(leagueCode, args.week)
+        core.logger.info("Set week to (" + str(args.week) + ") for league (" + leagueCode + ")")
 
 def synchGames(args):
-    synchGamesHandler = pickemSynchGames.PickemSynchGamesHandler(apiClient, logger)
+    synchGamesHandler = pickemSynchGames.PickemSynchGamesHandler(core.apiClient, core.logger)
 
     if ( args.loop_every_sec == None ):
         synchGamesHandler.Run(args.action, args.ncaa_season_code, args.pickem_season_code, args.week, args.game_source, args.dump_json)
@@ -82,35 +68,27 @@ def synchGames(args):
            
             runCount = runCount + 1
 
-            logger.debug("-- Run #" + str(runCount) + " complete. Fail count - " + str(failCount) + ". Snoozing " + str(args.loop_every_sec) + " seconds")
+            core.logger.debug("-- Run #" + str(runCount) + " complete. Fail count - " + str(failCount) + ". Snoozing " + str(args.loop_every_sec) + " seconds")
             time.sleep(args.loop_every_sec)
 
 def updateGame(args):
     # read current data. Then update ONLY the title and re-send the rest of the current state
     # TODO: better way to do this?
-    gameData = apiClient.readGame(args.game_id)
-    apiClient.updateGame(args.game_id, gameData['gameStart'], gameData['lastUpdated'], gameData['gameState'], gameData['currentPeriod'], gameData['timeClock'], gameData['awayTeam']['score'], gameData['homeTeam']['score'], args.game_title)
+    gameData = core.apiClient.readGame(args.game_id)
+    core.apiClient.updateGame(args.game_id, gameData['gameStart'], gameData['lastUpdated'], gameData['gameState'], gameData['currentPeriod'], gameData['timeClock'], gameData['awayTeam']['score'], gameData['homeTeam']['score'], args.game_title)
 
 def updateSpreads(args):
-    synchGamesHandler = pickemUpdateSpreads.PickemUpdateSpreadsHandler(apiClient, logger)
+    synchGamesHandler = pickemUpdateSpreads.PickemUpdateSpreadsHandler(core.apiClient, core.logger)
     synchGamesHandler.Run(args.action, args.pickem_season_code, args.week)
 
 def updateTeams(args):
-    updateTeamsHandler = pickemUpdateTeams.PickemUpdateSpreadsHandler(apiClient, logger)
+    updateTeamsHandler = pickemUpdateTeams.PickemUpdateSpreadsHandler(core.apiClient, core.logger)
     updateTeamsHandler.Run(args.pickem_season_code, args.week, args.rankings_source)
 
 
 #=====================================
 # Helper functions
 #=====================================
-def loadIniConfig(iniFile, settingsContainer):
-    configParser = configparser.ConfigParser()
-    configParser.read(iniFile)
-    settingsContainer.MinLogLevelToApi = configParser.get("LOGGING", "MIN_LOG_LEVEL_TO_API")
-    settingsContainer.PickemAdminUsername = configParser.get("ADMIN", "PICKEM_ADMIN_USERNAME")
-    settingsContainer.PickemAdminPassword = configParser.get("ADMIN", "PICKEM_ADMIN_PASSWORD")
-    settingsContainer.PickemServerBaseUrl = configParser.get("URLS", "PICKEM_SERVER_BASE_URL")
-
 def setupArgumentParsers():
     # arg setup
     argParser = argparse.ArgumentParser()
@@ -182,18 +160,13 @@ print("----------------------------------------")
 print("  Pickem Console - {0:s} ".format(VERSION))
 print("----------------------------------------")
 
-loadIniConfig(PICKEM_INI, settings)
 argParser = setupArgumentParsers()
 
 # parse and run the subcommand
 args = argParser.parse_args()
 if ( hasattr(args, 'func') ):
 
-    logger = pickemLogger.Logger(settings.PickemServerBaseUrl, settings.MinLogLevelToApi, args.func.__name__)
-    apiClient = pickemApiClient.PickemApiClient(settings.PickemServerBaseUrl, logger)
-
-    # login
-    apiClient.authenticate(settings.PickemAdminUsername, settings.PickemAdminPassword)
+    core = pickemCore.Core(args.func.__name__)
 
     # this runs the function in parser's set_defaults()
     args.func(args)
