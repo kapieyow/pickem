e80eaff019cce4b2e7e8b22a1c36b05d1e677eef
kapieyow <33786261+kapieyow@users.noreply.github.com>
2019-08-21 20:06:08 -0400

    wrap for 2.1 deploy

--- pickem-scripts/run-once-setup-2019-NCAAF.py
+++ pickem-scripts/run-once-setup-2019-NCAAF.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 import pickemCore
 import pickemSynchGames
-import pickemUpdateSpreads
-import pickemUpdateTeams
 import subprocess
 
 core = pickemCore.Core("Run once - Setup 2019 NCAAF Leagues")
@@ -28,34 +26,6 @@ synchGamesHandler.Run("insert", 2019, 19, 14, False)
 currentLeagueCode = ""
 currentUserName = ""
 currentPlayerTag = ""
-
-# !!!!!!!!!!!!!!!!! -- this part is TEST only cut before prod !!!!!!!!!!!!!!!!!
-# -- TEST league for "week 0" -----------------
-currentLeagueCode = "TEST-NCAAF-Week-0-19"
-core.apiClient.insertLeague(1, currentLeagueCode, "Test of Week 0 - 2019", "2019", "AllWinsOnePoint", "19", [1])
-
-currentUserName = "kip"
-currentPlayerTag = "Kip"
-core.apiClient.addPlayerToLeague(currentLeagueCode, currentUserName, currentPlayerTag)
-core.apiClient.updateUserAccount(currentUserName, currentLeagueCode)
-
-# add week "0" games are really in week 1 but the early weekend
-core.setLeagueGames([currentLeagueCode], 1, [401110723, 401114164])
-
-# ###**AP not posted yet for 2019** pickem.py update_teams -rs ap -psc 19 -w 1
-# subprocess.call("pickem.py update_teams -rs ap -psc 19 -w 1", shell=True)
-# core.logger.info("Updated teams for week \"0\"")
-# updateTeamsHandler = pickemUpdateTeams.PickemUpdateSpreadsHandler(core.apiClient, core.logger)
-# updateTeamsHandler.Run(19, 1, "ap")
-
-# update spreads for week one
-synchGamesHandler = pickemUpdateSpreads.PickemUpdateSpreadsHandler(core.apiClient, core.logger)
-synchGamesHandler.Run("update", 19, 1)
-
-# /!!!!!!!!!!!!!!!!! end TEST garbage !!!!!!!!!!!!!!!!!
-
-
-
 
 # -- NeOnYa 2019 -----------------
 currentLeagueCode = "NeOnYa-NCAAF-19"
@@ -124,7 +94,7 @@ core.apiClient.updateUserAccount(currentUserName, currentLeagueCode)
 
 # -- Burl Mafia 2019 ------------------
 currentLeagueCode = "BurlMafia-NCAAF-19"
-core.apiClient.insertLeague(1, currentLeagueCode, "Burl Mafia - 2019", "2019", "AllWinsOnePoint", "19", [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])
+core.apiClient.insertLeague(1, currentLeagueCode, "Burlington Mafia - 2019", "2019", "AllWinsOnePoint", "19", [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])
 
 currentUserName = "kip"
 currentPlayerTag = "Kip"
