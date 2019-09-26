01e8a9ffc662b176a18580bbb5f3c57494c9d3ff
kapieyow <33786261+kapieyow@users.noreply.github.com>
2019-09-26 19:06:45 -0400

    Better JWT issue logging and other cleanup

--- pickem-web/src/app/sub-system/auth/auth-guard.ts
+++ pickem-web/src/app/sub-system/auth/auth-guard.ts
@@ -2,16 +2,26 @@ import { JwtHelperService } from '@auth0/angular-jwt';
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 
+import { LoggerService } from '../services/logger.service';
+
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private jwtHelper: JwtHelperService, private router: Router) {
+  constructor(private jwtHelper: JwtHelperService, private router: Router, private logger: LoggerService) {
   }
   canActivate() {
     var token = localStorage.getItem("JWT");
 
-    if (token && !this.jwtHelper.isTokenExpired(token)){
-      console.log(this.jwtHelper.decodeToken(token));
-      return true;
+    if ( token ) {
+      if ( !this.jwtHelper.isTokenExpired(token) ) {
+        return true;
+      }
+      else {
+        this.logger.warn("JWT expired. Rerouting to login.")
+      }
+    }
+    else
+    {
+      this.logger.warn("No JWT present. Rerouting to login.")
     }
     this.router.navigate(['/'], { skipLocationChange: true });
     return false;
