5fdabe93224bda545c83b069c731ed05054bc38b
kapieyow <33786261+kapieyow@users.noreply.github.com>
2019-12-02 07:59:26 -0500

    bot league setup

--- pickem-scripts/run-once-setup-bot-league.py
+++ pickem-scripts/run-once-setup-bot-league.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3	
 import pickemCore	
 import pickemAddYahooGames
-import subprocess
 
 core = pickemCore.Core("Run once - Setup 2019 NCAAF Bot League")	
 
 currentLeagueCode = "BOTS-NCAAF-19"		
-currentLeagueWeek = 9
+currentLeagueWeek = 8
 currentUserName = ""	
 currentPlayerTag = ""
 
 
 # -- Bot League 2019 -----------------	
-core.apiClient.insertLeague(currentLeagueWeek, currentLeagueCode, "Pickbots - 2019", "2019", "AllWinsOnePoint", "19", [9,10,11,12,13,14,15])	
+core.apiClient.insertLeague(currentLeagueWeek, currentLeagueCode, "Pickbots - 2019", "2019", "AllWinsOnePoint", "19", [8,9,10,11,12,13,14,15])	
 
 currentUserName = "kip"	
 currentPlayerTag = "sigterm"	
@@ -35,5 +34,8 @@ currentPlayerTag = "AgBot"
 core.apiClient.addPlayerToLeague(currentLeagueCode, currentUserName, currentPlayerTag)	
 
 currentUserName = "PickBot"	
-currentPlayerTag = "Pickbot 9000 👾"	
+currentPlayerTag = "Pickbot 9000"	
 core.apiClient.addPlayerToLeague(currentLeagueCode, currentUserName, currentPlayerTag)	
+
+pickemAddYahooGamesHandler = pickemAddYahooGames.PickemAddYahooGamesHandler(core)
+pickemAddYahooGamesHandler.Run([currentLeagueCode], currentLeagueWeek)
