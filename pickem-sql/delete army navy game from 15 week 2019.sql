7a64741182c579175d5dff654979a8612d3f990c
kapieyow <33786261+kapieyow@users.noreply.github.com>
2020-08-13 19:09:05 -0400

    week 15 2019 game drop

---
+++ pickem-sql/delete army navy game from 15 week 2019.sql
@@ -0,0 +1,30 @@
+﻿BEGIN;
+UPDATE
+	public.mt_doc_leaguedata l
+SET 
+	data = l.data #- '{Weeks,14,Games,6}' -- ZERO based indexes, delete 7th game from 15th week ... which was army/navy and not really in the same week
+WHERE
+	l.data->>'LeagueCode' = 'BUS-NCAAF-19'
+;
+UPDATE
+	public.mt_doc_leaguedata l
+SET 
+	data = l.data #- '{Weeks,14,Games,6}' -- ZERO based indexes, delete 7th game from 15th week ... which was army/navy and not really in the same week
+WHERE
+	l.data->>'LeagueCode' = 'BurlMafia-NCAAF-19'
+;
+UPDATE
+	public.mt_doc_leaguedata l
+SET 
+	data = l.data #- '{Weeks,14,Games,6}' -- ZERO based indexes, delete 7th game from 15th week ... which was army/navy and not really in the same week
+WHERE
+	l.data->>'LeagueCode' = 'NeOnYa-NCAAF-19'
+;
+UPDATE
+	public.mt_doc_leaguedata l
+SET 
+	data = l.data #- '{Weeks,6,Games,6}' -- ZERO based indexes, delete 7th game from 15th week which is only the 6th in this league ... which was army/navy and not really in the same week
+WHERE
+	l.data->>'LeagueCode' = 'BOTS-NCAAF-19'
+;
+
