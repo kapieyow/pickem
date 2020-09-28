fad5ea65b77acb38a3e177e7747c5133970eed4a
kapieyow <33786261+kapieyow@users.noreply.github.com>
2020-09-28 17:49:36 -0400

    correction script on default leagues

---
+++ pickem-scripts/run-once-correct-def-leagues.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3	
+import pickemCore	
+
+core = pickemCore.Core("Correct 2020 Burlington Mafia default leagues")	
+
+core.apiClient.updateUserAccount("CusheGoblin", "BurlMafia-NCAAF-20")
+core.apiClient.updateUserAccount("Cary", "BurlMafia-NCAAF-20")
+core.apiClient.updateUserAccount("fvedder", "BurlMafia-NCAAF-20")
+core.apiClient.updateUserAccount("tobyschau2@yahoo.com", "BurlMafia-NCAAF-20")
